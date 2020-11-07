3335fdeae728bd22c18a9270a2976df6b1c861e3
Kindeme <fkounoudji@gmail.com>
2020-11-07 00:02:04 -0600

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,18 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'oneConversion';
   showMsg = false;
-  switchValue = "three";
-  users =[
-    {id: 1, name:'john'},
-    {id: 2, name:'Flore'}
+  switchValue = 'three';
+  users = [
+    { userId: 10, name: 'John' },
+    { userId: 20, name: 'Flore' },
+    { userId: 30, name: 'Marie' },
+    { userId: 40, name: 'Tiffany' },
+    { userId: 50, name: 'Hillary' },
+    { userId: 23, name: 'Ryan' },
   ];
-    
-  
-
 }
